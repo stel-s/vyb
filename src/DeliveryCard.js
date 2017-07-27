3ed27bd4cb76d0ea9ae39f52bdb4bc579ed57a12
stel-s <stelios.sideras@gmail.com>
2017-07-27 09:02:35 +0300

    fD

--- src/DeliveryCard.js
+++ src/DeliveryCard.js
@@ -15,19 +15,28 @@ const style = {
 };
 class DeliveryCard extends Component {
     state = {
-        isFrozen: true
-    }
+        isFrozen: true,
+        storeName: this.props.order.stores[0].full_name,
+        name: this.props.order.hours[0].delivery_runs[0].orders[0].customer.name,
+        ack: false,
+    };
+
+    handleClick = () => {
+      this.setState({ack: true})
+    };
 
     render() {
+        console.log(this.props.order.hours[0].delivery_runs[0].orders[0].customer.name)
+
         return (
             <div>
                 <Paper style={{...style,backgroundColor: this.props.color}} zDepth={2}>
-                    whole foods
+                    {this.state.storeName}
                     {this.state.isFrozen && <div style={{float: 'right'}}>
                         <img width="25" height="25" src={frozen} />
                     </div>}
                     <div style={{display:'flex',flexDirection: 'column', lineHeight: '23px'}}>
-                        <div  style={{alignSelf:'flex-start', fontWeight: '600', color:'rgba(0, 0, 0, 0.64)'}}>Name Surname</div>
+                        <div  style={{alignSelf:'flex-start', fontWeight: '600', color:'rgba(0, 0, 0, 0.64)'}}>{this.state.name}</div>
                         <div  style={{alignSelf:'flex-start', color: 'green'}}>$15.37</div>
                         <div  style={{alignSelf:'flex-start'}}>
                             Items: &nbsp;
@@ -41,7 +50,7 @@ class DeliveryCard extends Component {
                         <div  style={{alignSelf:'flex-start',opacity:'0.6'}}>Delivery Time <span style={{color:'red'}}>17min</span></div>
                     </div>
 
-                    <RaisedButton style={{marginTop: '30px'}} label="acknowledge"  />
+                    <RaisedButton style={{marginTop: '30px', }} backgroundColor = {this.state.ack ? 'green':''} label="acknowledge"  onClick={this.handleClick} />
 
                 </Paper>
             </div>
